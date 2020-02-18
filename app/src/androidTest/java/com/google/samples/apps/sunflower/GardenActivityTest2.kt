a5c56018e97417575d2b92edde5c1c0457c20e6e
TOYAMA Sumio <jun.nama@gmail.com>
2020-02-18 20:00:46 +0900

    Add some idling resources for more stability

--- app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest2.kt
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest2.kt
@@ -1,10 +1,18 @@
 package com.google.samples.apps.sunflower
 
 
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.android.example.github.util.TaskExecutorWithIdlingResourceRule
+import com.example.android.architecture.blueprints.todoapp.util.DataBindingIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.monitorActivity
+import com.google.samples.apps.sunflower.data.AppDatabase
 import com.google.samples.apps.sunflower.page.MyGardenPage
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,7 +22,24 @@ import org.junit.runner.RunWith
 class GardenActivityTest2 {
 
     @get:Rule
-    var activityScenarioRule = activityScenarioRule<GardenActivity>()
+    val activityScenarioRule = activityScenarioRule<GardenActivity>()
+
+    @get:Rule
+    val taskExecutorWithIdlingResourceRule = TaskExecutorWithIdlingResourceRule()
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun setUp() {
+        val idlingRegistry = IdlingRegistry.getInstance()
+        dataBindingIdlingResource.monitorActivity(activityScenarioRule.scenario)
+        idlingRegistry.register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
 
     @Test
     fun gardenActivityTest2() {
